d65e0ca203e7725423cc2d6af1a9cb0be7bd5b58
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2022-11-23 08:39:49 +0100

    ended reverse params

--- eau02.js
+++ eau02.js
@@ -1,24 +1,61 @@
-// returns multiple strings from last to first
-const returnString = () => {
-  const string = process.argv;
+/********************
+ *     Functions     *
+ ********************/
 
-  string.splice(0, 2);
-  if (string.length != 1) {
-    return "error";
+// returns multiple strings from last to first
+const returnString = (text) => {
+  const bundleText = [];
+  text.forEach((element) => {
+    bundleText.push(element);
+  });
+
+  return bundleText;
+};
+
+/********************
+ *      Parsing      *
+ ********************/
+
+const parsing = () => {
+  const text = process.argv;
+
+  text.splice(0, 2);
+  if (text.length < 1) {
+    errorLog();
+  } else {
+    return returnString(text);
+  }
+};
+
+/**********************
+ *   Error management  *
+ **********************/
+const errorLog = () => {
+  console.log("error, closure of the terminal");
+  process.exit();
+};
+
+/****************
+ *   Formatting  *
+ ****************/
+const displayArray = (result) => {
+  let stringResult = "";
+
+  for (let i = result.length; i > 1; i--) {
+    stringResult += `${result[i - 1]}\n`;
   }
 
-  const array = [];
+  stringResult += result[0];
 
-  return string;
+  console.log(stringResult);
 };
 
-// Formatting of the result as a string
-const displayArray = (result) => {
-  console.log(result);
-};
+/********************
+ *        Main       *
+ ********************/
+const result = parsing();
 
-// To start the program
-const result = returnString();
-
-// To stringify the result
+/********************
+ *       Result      *
+ ********************/
 displayArray(result);
